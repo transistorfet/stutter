c458e58f2876f33023d075bf5ad076640134fd7c
trans <trans@jabberwocky.ca>
2005-09-20 16:06:53 +0000

    If already in the given channel, the window for that channel is made current.  If the join command fails, the window is destroyed.

--- src/modules/irc/cmds/join.c
+++ src/modules/irc/cmds/join.c
@@ -12,11 +12,17 @@ int irc_cmd_join(char *env, char *str)
 {
 	void *window;
 	struct irc_server *server;
+	struct irc_channel *channel;
 
 	if ((*str == '\0') || !(server = irc_current_server()))
 		return(-1);
-	if (window = fe_create_widget("irc:window", fe_get_parent(fe_current_widget()))) {
-		irc_join_channel(server, str, window);
+	if (channel = irc_get_channel(server, str))
+		fe_set_current_widget(channel->window);
+	else if (window = fe_create_widget("irc:window", fe_get_parent(fe_current_widget()))) {
+		if (!irc_join_channel(server, str, window)) {
+			fe_destroy_widget(window);
+			return(-1);
+		}
 		fe_set_current_widget(window);
 	}
 	return(0);
