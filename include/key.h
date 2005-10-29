e75b865f8b7c052fe08d846fc832a1180c21129f
trans <trans@jabberwocky.ca>
2005-10-29 18:12:38 +0000

    Added keybinding functions.

---
+++ include/key.h
@@ -0,0 +1,21 @@
+/*
+ * Header Name:		key.h
+ * Version:		0.1
+ * Description:		Key Manager Header
+ */
+
+#ifndef _KEY_H
+#define _KEY_H
+
+#include <nit/string.h>
+#include <nit/callback.h>
+
+int init_key(void);
+int release_key(void);
+
+int bind_key(char *context, char *key, struct callback_s *, string_t);
+int unbind_key(char *context, char *key);
+int process_key(int ch);
+
+#endif
+
