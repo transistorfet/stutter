e7226e066961f4750b4522335ff0d3a20d234643
trans <trans@jabberwocky.ca>
2005-10-29 20:49:36 +0000

    Added select_key_context function.

--- include/stutter/key.h
+++ include/stutter/key.h
@@ -16,6 +16,7 @@ int release_key(void);
 int bind_key(char *context, char *key, struct callback_s *, string_t);
 int unbind_key(char *context, char *key);
 int process_key(int ch);
+int select_key_context(char *);
 
 #endif
 
