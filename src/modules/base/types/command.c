d0854d3cad4bb5a08703ad6146c00cc73253b4a2
trans <trans@jabberwocky.ca>
2005-11-11 16:09:24 +0000

    Added new type create function and removed old public add_command function.

--- src/modules/base/types/command.c
+++ src/modules/base/types/command.c
@@ -6,24 +6,35 @@
  */
 
 #include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
 
 #include <type.h>
 #include <variable.h>
 #include <nit/callback.h>
 
-static struct type_s *cmd_type;
+static void *base_command_create(char *, ...);
 
 int base_load_command(void)
 {
-	if (!(cmd_type = add_type("command", NULL, (evaluate_t) execute_callback, (destroy_t) destroy_callback)))
+	if (!(add_type("command", (create_t) base_command_create, (evaluate_t) execute_callback, (destroy_t) destroy_callback)))
 		return(-1);
 	return(0);
 }
 
-int base_add_command(char *ns_name, char *var_name, callback_t func, void *ptr)
+static void *base_command_create(char *str, ...)
 {
-	if (!var_name || !func)
-		return(-1);
-	return(!add_variable(cmd_type, ns_name, var_name, (void *) create_callback(0, 0, NULL, func, ptr)));
+	void *ptr;
+	va_list va;
+	callback_t func;
+
+	if (!strncmp(str, "%r%", 3) && ((str[3] == 'p') || (str[3] == 's') || (str[3] == 'd') || (str[3] == 'c')) && (str[4] == '\0')) {
+		va_start(va, str);
+		func = va_arg(va, callback_t);
+		ptr = va_arg(va, void *);
+		return((void *) create_callback(0, 0, NULL, func, ptr));
+	}
+	else
+		return(NULL);
 }
 
