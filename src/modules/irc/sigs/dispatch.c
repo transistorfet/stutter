fa6c0f9647c3bd68880cd5ca9d84d791df621058
trans <trans@jabberwocky.ca>
2005-11-11 16:07:28 +0000

    Modified to use new dispatch function format where the server struct is passed as the first argument.

--- src/modules/irc/sigs/dispatch.c
+++ src/modules/irc/sigs/dispatch.c
@@ -8,30 +8,30 @@
 #include CONFIG_H
 #include <modules/irc.h>
 
-int irc_msg_dispatch(char *env, struct irc_msg *msg)
+int irc_dispatch_msg(struct irc_server *server, struct irc_msg *msg)
 {
 	switch (msg->cmd) {
 		case IRC_MSG_NOTICE:
-			return(irc_msg_notice(env, msg));
+			return(irc_msg_notice(server, msg));
 		case IRC_MSG_JOIN:
-			return(irc_msg_join(env, msg));
+			return(irc_msg_join(server, msg));
 		case IRC_MSG_PART:
-			return(irc_msg_part(env, msg));
+			return(irc_msg_part(server, msg));
 		case IRC_MSG_PRIVMSG:
-			return(irc_msg_privmsg(env, msg));
+			return(irc_msg_privmsg(server, msg));
 		case IRC_RPL_NAMREPLY:
-			return(irc_msg_names(env, msg));
+			return(irc_msg_names(server, msg));
 		case IRC_RPL_TOPIC:
 		case IRC_MSG_TOPIC:
-			return(irc_msg_topic(env, msg));
+			return(irc_msg_topic(server, msg));
 		case IRC_MSG_KICK:
-			return(irc_msg_kick(env, msg));
+			return(irc_msg_kick(server, msg));
 		case IRC_MSG_NICK:
-			return(irc_msg_nick(env, msg));
+			return(irc_msg_nick(server, msg));
 		case IRC_MSG_QUIT:
-			return(irc_msg_quit(env, msg));
+			return(irc_msg_quit(server, msg));
 		default:
-			return(irc_msg_default(env, msg));
+			return(irc_msg_default(server, msg));
 	}
 
 	return(0);
