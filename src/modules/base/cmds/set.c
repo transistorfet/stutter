976e8bad181bef43946b9cebc4cbe2017000fb40
trans <trans@jabberwocky.ca>
2005-12-09 06:20:22 +0000

    Modified to take -type as a parameter and use the create function of that type.

--- src/modules/base/cmds/set.c
+++ src/modules/base/cmds/set.c
@@ -1,13 +1,15 @@
 /*
  * Command Name:	set.c
  * Version:		0.1
- * Module Requirements:	type ; variable ; frontend ; string ; modbase
+ * Module Requirements:	type ; utils ; variable ; frontend ; string ; modbase
  * Description:		Set Variable Command
  */
 
 #include <string.h>
 
+#include CONFIG_H
 #include <type.h>
+#include <utils.h>
 #include <macros.h>
 #include <variable.h>
 #include <frontend.h>
@@ -16,18 +18,30 @@
 
 int base_cmd_set(char *env, char *args)
 {
+	int j = 0;
 	void *window;
-	string_t value;
 	char *ns, *name;
+	struct type_s *type;
+	char *type_name = BASE_DEFAULT_SET_TYPE;
 
-	get_param_m(args, name, ' ');
-	if (!(window = fe_next_widget()) && !(window = fe_first_widget()))
+
+	if (!(window = fe_next_widget("window", NULL)) && !(window = fe_first_widget("window", NULL)))
 		return(-1);
+
+	trim_whitespace_m(args);
+	if (*args == '-')
+		split_string_m(&args[1], type_name, args, ' ');
+	get_param_m(args, name, ' ');
 	get_prefix_m(name, ns, ':');
-	if (add_variable(find_type("string"), ns, name, create_string(args)))
-		fe_print(window, create_string("Variable: %s:%s <= %s", ns ? ns : "", name, args));
-	else
+
+	trim_whitespace_m(args);
+	if (!(type = find_type(type_name)) || !type->create)
+		fe_print(window, create_string("Error: Type %s not found.", type_name));
+	else if (!add_variable(type, ns, name, type->create("%s", args)))
 		fe_print(window, create_string("Error setting variable."));
+	else
+		// TODO should the variable be stringified and then printed instead of printing args directly?
+		fe_print(window, create_string("Variable: %s:%s <= %s", ns ? ns : "", name, args));
 	return(0);
 }
 
