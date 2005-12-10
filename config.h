c6854a02371606dc16af436f7b70f85eb97109c3
trans <trans@jabberwocky.ca>
2005-12-10 06:29:08 +0000

    Updated for general changes.

--- config.h
+++ config.h
@@ -6,11 +6,32 @@
 #ifndef _CONFIG_H
 #define _CONFIG_H
 
+#ifdef BUILTIN_CONFIG
+	#define DECLARE_CONFIG(name, value)	// save value?!??
+	#define CONFIG_VALUE(name)		name
+#else
+
+#endif
+
+#define FE_NAMESPACE			"fe"
 #define BASE_NAMESPACE			"base"
-
 #define IRC_NAMESPACE			"irc"
+#define VARIABLE_PATH			"irc;base"
 
-#define IRC_DEFAULT_SERVER		"euro.othersideirc.net"
+/* Frontend Values */
+#define FE_STATUS			"status"
+#define FE_STATUS_BAR			" $time $irc:nick on $irc:current_channel"
+#define FE_STATUS_BAR_HEIGHT		1
+
+/* Base Module Values */
+#define BASE_TIME			"%H:%M"
+#define BASE_DATE			"%d-%b"
+#define BASE_TIMESTAMP			"%d-%b:%H:%M"
+#define BASE_DEFAULT_SET_TYPE		"string"
+
+/* IRC Module Values */
+#define IRC_DEFAULT_SERVER		"irc.othersideirc.net"
+//#define IRC_DEFAULT_SERVER		"backdraft.othersideirc.net"
 //#define IRC_DEFAULT_SERVER		"clarke.freenode.net"
 #define IRC_DEFAULT_PORT		6667
 #define IRC_DEFAULT_NICK		"ted"
@@ -21,16 +42,12 @@
 
 #define IRC_CHANNEL_BITFLAGS		0x01
 
-#define IRC_STATUS_BAR			" %H %n on %c (%t)"
-#define IRC_STATUS_COLOUR		SC_INVERSE
-
 #define IRC_TIMESTAMP			"%d-%b:%H:%M"
 #define IRC_BANNER			"***"
 
 #define IRC_FMT_DEFAULT			"%N!%S %C %M"
 #define IRC_FMT_ACTION			"[%@] * %N %M"
 #define IRC_FMT_ACTION_SELF		"[%@] * %n %M"
-//#define IRC_FMT_PRIVMSG		"&timestamp; &msg;"
 #define IRC_FMT_PRIVMSG			"[%@ %N] %M"
 #define IRC_FMT_PRIVMSG_SELF		"[%@ %n] %M"
 #define IRC_FMT_NOTICE			"%B [%@] -%N- %M"
@@ -38,8 +55,8 @@
 #define IRC_FMT_JOIN			"%B [%@] Joined %P1: %N (%S)"
 #define IRC_FMT_PART			"%B [%@] Leaving %P1: %N (%M)"
 #define IRC_FMT_QUIT			"%B [%@] Quit: %N (%M)"
-#define IRC_FMT_TOPIC			"%B Topic Set To: %M"
-//#define IRC_FMT_NAMES			"&banner; Users on <channel>%P3</channel>: &msg;"
+#define IRC_FMT_TOPIC			"%B Topic Set by %N To: %M"
+//#define IRC_FMT_NAMES			"$banner Users on <channel>%P3</channel>: <msg>%T</msg>"
 #define IRC_FMT_NAMES			"%B Users on %P3: %M"
 #define IRC_FMT_KICK			"%B [%@] %P2 was kicked from %P1 by %N (%M)"
 #define IRC_FMT_KICK_SELF		"%B [%@] You were kicked from %P1 by %N (%M)"
@@ -47,18 +64,24 @@
 
 #define IRC_QUIT_MSG			"The Pooper Scooper Of Life!"
 
-/** Name of function to callback when a msg is received or NULL to disable irc (default: base_msg_dispatch) */
-#define DISPATCH_CALLBACK		irc_msg_dispatch
-
+/* General Values */
 #define UNKNOWN_COMMAND			"*** Unknown Command"
 #define COMMAND_PREFIX			"/"
 #define DEFAULT_COMMAND			""
-#define COMMAND_PARSER			base_cmd_parse
 	
 //#define GREET_MSG			"Welcome to the show!"
 
 #define DOTFILE_DIR			".stutter"
 #define DEFAULT_DOTFILE_DIR		"/usr/local/etc"
+
+
+
+//#define IRC_CONFIG()						\
+//	DECLARE_CONFIG(IRC_FMT_DEFAULT, "%N!%S %C %M")		\
+
+
+// GET_CONFIG(IRC_FMT_DEFAULT)
+// TODO the problem with this is that we either make all configs vars or all compiled in
 
 #endif
 
