3ab3b6ea40d56bc42b0c35e40d40d0885be1f2d5
trans <trans@jabberwocky.ca>
2005-12-10 05:24:35 +0000

    Changed most of the frontend API to be more usable.

--- include/stutter/frontend.h
+++ include/stutter/frontend.h
@@ -10,39 +10,29 @@
 #include <nit/string.h>
 #include <nit/callback.h>
 
-void *fe_create_widget(char *, void *);
-int fe_destroy_widget(void *);
-void *fe_get_parent(void *);
-short fe_get_width(void *);
-short fe_get_height(void *);
+void *fe_create_widget(char *ns, char *type, char *id, void *parent);
+int fe_destroy_widget(void *widget);
+void *fe_get_parent(void *widget);
+short fe_get_width(void *widget);
+short fe_get_height(void *widget);
+void *fe_find_widget(char *id);
+int fe_resize_widget(void *widget, int diffx, int diffy);
 
-// TODO what argument will we pass to all of these functions in order to specify a "context" (a string? a widget?)
-void *fe_current_widget(void);
-// TODO add an fe_find_widget function to find a widget based on a name or number (or both?)
-int fe_select_widget(void *);
-void *fe_next_widget(void);
-void *fe_previous_widget(void);
-void *fe_first_widget(void);
-void *fe_last_widget(void);
+void *fe_current_widget(char *context, void *ref);
+int fe_select_widget(char *context, void *ref, void *widget);
+void *fe_next_widget(char *context, void *ref);
+void *fe_previous_widget(char *context, void *ref);
+void *fe_first_widget(char *context, void *ref);
+void *fe_last_widget(char *context, void *ref);
 
-int fe_print(void *, string_t);
-void fe_clear(void *);
-void fe_move(void *, short, short);
-int fe_scroll(void *, int);
-void fe_refresh(void);
+int fe_print(void *widget, string_t str);
+char *fe_read(void *widget);
+void fe_clear(void *widget);
+void fe_move(void *widget, short x, short y);
+int fe_scroll(void *widget, int diff);
+void fe_refresh(void *widget);
 
-// TODO should this function return the acutal buffer or a string_t?
-char *fe_get_input(void);
-int fe_set_input(char *);
-void fe_clear_input(void);
-
-// TODO there should only be one way to specify a callback and that method should be consistent (register vs bind_key)
-// TODO should there be more callbacks or something passed to fe_register_widget
-int fe_register_widget(char *, struct callback_s *);
-int fe_unregister_widget(char *);
-
-// TODO is this good or should it *really* be fe_exit()?
-void fe_terminate(void);
+void fe_terminate(int status);
 
 #endif
 
