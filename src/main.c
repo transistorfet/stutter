413fe850f4344420a53b833502d9c1075a11e680
trans <trans@jabberwocky.ca>
2005-12-10 06:29:43 +0000

    Broken initialization into 2 different sets of functions.

--- src/main.c
+++ src/main.c
@@ -1,47 +1,60 @@
 /*
  * Module Name:		main.c
  * Version:		0.1
- * Module Requirements:	type ; variable ; frontend ; modirc ; modbase
+ * Module Requirements:	type ; utils ; variable ; frontend ; modirc ; modbase
  * Description:		IRC Client
  */
 
 #include CONFIG_H
 #include <type.h>
+#include <utils.h>
 #include <variable.h>
 #include <frontend.h>
 #include <modules/irc.h>
 #include <modules/base.h>
 
-#include <nit/callback.h>
+#include <nit/string.h>
 
 int init_system(void)
 {
-	void *window;
-
 	init_key();
 	init_variable();
+
+	add_type("string", (create_t) create_string, (stringify_t) duplicate_string, NULL, (destroy_t) destroy_string);
+	add_type("format", (create_t) create_string, (stringify_t) util_expand_str, NULL, (destroy_t) destroy_string);
+
+	return(0);
+}
+
+int release_system(void)
+{
+	release_variable();
+	release_key();
+	return(0);
+}
+
+int load_modules(void)
+{
+	void *window;
 
 	/* Load Modules */
 	init_base();
 	init_irc();
 
-	select_variable_path("irc;base");
+	select_variable_path(VARIABLE_PATH);
 
 	/* Setup Base Module */
 	bind_key(NULL, "\n", find_variable(NULL, NULL, "parse"), create_string(""));
 	bind_key(NULL, "\x18", find_variable(NULL, NULL, "next"), create_string(""));
-
-	/* Setup IRC Module */
-	fe_register_widget("irc:statusbar", create_callback(0, 0, NULL, (callback_t) irc_status_bar, NULL));
-
-	if (!(window = fe_create_widget("irc:window", fe_get_parent(fe_current_widget()))))
-		return(-1);
+	bind_key(NULL, "\x11", find_variable(NULL, NULL, "previous"), create_string(""));
 
 	#ifdef GREET_MSG
 	fe_print(window, create_string(GREET_MSG));
 	#endif
 
 	#ifdef IRC_DEFAULT_SERVER
+	if (!(window = fe_create_widget("irc", "window", NULL, NULL)))
+		return(-1);
 	if (!irc_server_connect(IRC_DEFAULT_SERVER, IRC_DEFAULT_PORT, IRC_DEFAULT_NICK, window))
 		fe_print(window, create_string("Error connecting to server %s on port %d", IRC_DEFAULT_SERVER, IRC_DEFAULT_PORT));
 	#endif
@@ -49,17 +62,11 @@ int init_system(void)
 	return(0);
 }
 
-
-int release_system(void)
+int unload_modules(void)
 {
 	/* Release Modules */
 	release_irc();
 	release_base();
-
-	release_variable();
-	release_key();
 	return(0);
 }
-
-
 
