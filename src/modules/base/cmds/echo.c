e2dc5ab3f0839b77d2498c8cc63847bb782d0ed6
trans <trans@jabberwocky.ca>
2005-12-15 01:20:08 +0000

    Added echo command to print the argument to the screen.

---
+++ src/modules/base/cmds/echo.c
@@ -0,0 +1,22 @@
+/*
+ * Command Name:	echo.c
+ * Version:		0.1
+ * Module Requirements:	frontend ; modbase
+ * Description:		Echo Command
+ */
+
+
+#include <frontend.h>
+#include <modules/base.h>
+
+int base_cmd_echo(char *env, char *args)
+{
+	void *window;
+
+	if (!(window = fe_next_widget("window", NULL)) && !(window = fe_first_widget("window", NULL)))
+		return(-1);
+	fe_print(window, create_string("%s", args));
+	return(0);
+}
+
+
