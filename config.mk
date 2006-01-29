8bde3ecec98247840ce380013d920ea3c6406585
trans <trans@jabberwocky.ca>
2006-01-29 07:13:26 +0000

    Removed libnit.

--- config.mk
+++ config.mk
@@ -6,8 +6,4 @@ MODULES = base irc
 CONFIG_H = ../config.h
 CFLAGS += -DDEBUG -DCONFIG_H='<$(CONFIG_H)>'
 
-LIBNIT_A = $(ROOT)/libnit
-
-LIBPATH += -L$(LIBNIT_A)/lib
-INCPATH += -I$(LIBNIT_A)/include
 
