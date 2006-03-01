fe055dfae84257d9dc57ac7ee07c6fbb74ada3eb
trans <trans@jabberwocky.ca>
2006-03-01 04:54:21 +0000

    Modified to not do the acutal window creation leaving it to the join msg handler instead.

--- src/modules/irc/cmds/join.c
+++ src/modules/irc/cmds/join.c
@@ -12,8 +12,8 @@
 int irc_cmd_join(char *env, char *args)
 {
 	char *name;
-	void *window;
 	struct irc_server *server;
+	struct irc_msg *msg = NULL;
 	struct irc_channel *channel;
 
 	get_param_m(args, name, ' ');
@@ -21,13 +21,8 @@ int irc_cmd_join(char *env, char *args)
 		return(-1);
 	if (channel = irc_find_channel(server->channels, name))
 		fe_select_widget("window", NULL, channel->window);
-	else if (window = fe_create_widget("irc", "window", NULL, NULL)) {
-		if (!irc_join_channel(server, name, window)) {
-			fe_destroy_widget(window);
-			return(-1);
-		}
-		fe_select_widget("window", NULL, window);
-	}
+	else
+		irc_join_channel(server, name);
 	return(0);
 }
 
