8167dc2c2d6cd3608959ea295314cf16c38aebb0
trans <trans@jabberwocky.ca>
2006-03-01 05:06:08 +0000

    Modified to create the window and add the channel to the channel list instead of being done when the join command is executed.

--- src/modules/irc/msgs/join.c
+++ src/modules/irc/msgs/join.c
@@ -14,9 +14,18 @@
 int irc_msg_join(struct irc_server *server, struct irc_msg *msg)
 {
 	char *str;
+	void *window;
 	struct irc_channel *channel;
 
-	if (!(channel = irc_find_channel(server->channels, msg->params[0])))
+	if (!strcmp(server->nick, msg->nick)) {
+		if (!(window = fe_create_widget("irc", "window", NULL, NULL)) || !(channel = irc_add_channel(server->channels, msg->params[0], window, server))) {
+			irc_leave_channel(server, msg->params[0]);
+			// TODO print error message
+			return(-1);
+		}
+		fe_select_widget("window", NULL, window);
+	}
+	else if (!(channel = irc_find_channel(server->channels, msg->params[0])))
 		return(-1);
 
 	irc_add_user(channel->users, msg->nick, 0);
