f332e4c371c3e1a3c9abec11dc66e9d0526d61e8
trans <trans@jabberwocky.ca>
2006-03-01 05:09:10 +0000

    Modified to use the new server signal dispatch method.

--- src/modules/irc/sigs/dispatch.c
+++ src/modules/irc/sigs/dispatch.c
@@ -12,26 +12,26 @@ int irc_dispatch_msg(struct irc_server *server, struct irc_msg *msg)
 {
 	switch (msg->cmd) {
 		case IRC_MSG_NOTICE:
-			return(irc_msg_notice(server, msg));
+			return(irc_msg_notice(msg->server, msg));
 		case IRC_MSG_JOIN:
-			return(irc_msg_join(server, msg));
+			return(irc_msg_join(msg->server, msg));
 		case IRC_MSG_PART:
-			return(irc_msg_part(server, msg));
+			return(irc_msg_part(msg->server, msg));
 		case IRC_MSG_PRIVMSG:
-			return(irc_msg_privmsg(server, msg));
+			return(irc_msg_privmsg(msg->server, msg));
 		case IRC_RPL_NAMREPLY:
-			return(irc_msg_names(server, msg));
+			return(irc_msg_names(msg->server, msg));
 		case IRC_RPL_TOPIC:
 		case IRC_MSG_TOPIC:
-			return(irc_msg_topic(server, msg));
+			return(irc_msg_topic(msg->server, msg));
 		case IRC_MSG_KICK:
-			return(irc_msg_kick(server, msg));
+			return(irc_msg_kick(msg->server, msg));
 		case IRC_MSG_NICK:
-			return(irc_msg_nick(server, msg));
+			return(irc_msg_nick(msg->server, msg));
 		case IRC_MSG_QUIT:
-			return(irc_msg_quit(server, msg));
+			return(irc_msg_quit(msg->server, msg));
 		default:
-			return(irc_msg_default(server, msg));
+			return(irc_msg_default(msg->server, msg));
 	}
 
 	return(0);
