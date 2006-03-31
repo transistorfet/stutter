8dd8d8eddbbe1b5ce3c1fa494559d323b6ffd090
trans <trans@jabberwocky.ca>
2006-03-31 04:58:54 +0000

    Modified to contain definition variables

--- config.mk
+++ config.mk
@@ -1,9 +1,16 @@
 
-
 FRONTEND = simple
 MODULES = base irc
 
 CONFIG_H = ../config.h
-CFLAGS += -DDEBUG -DCONFIG_H='<$(CONFIG_H)>'
+ARFLAGS += rc
+CFLAGS += -g -DDEBUG -DCONFIG_H='<$(CONFIG_H)>' -I$(ROOT)/include
 
+CC	= gcc
+CXX	= g++
+LD	= gcc
+AS	= as
+AR	= ar
+RANLIB	= ranlib
+DEPEND	= $(CC) -MM
 
