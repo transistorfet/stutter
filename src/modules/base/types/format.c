754b415a93fdb89296065de37a845ffb9d96ce19
trans <trans@jabberwocky.ca>
2006-05-04 03:54:05 +0000

    Moved from main.a.

---
+++ src/modules/base/types/format.c
@@ -0,0 +1,31 @@
+/*
+ * Type Name:		format.c
+ * Version:		0.1
+ * Module Requirements:	type ; utils ; string ; modbase
+ * Description:		Format String Type
+ */
+
+#include <stdlib.h>
+
+#include <stutter/type.h>
+#include <stutter/utils.h>
+#include <stutter/lib/string.h>
+#include <stutter/modules/base.h>
+
+struct type_s *base_load_format(void)
+{
+	return(add_type(
+		"format",
+		0,
+		(type_create_t) recreate_string_real,
+		(type_destroy_t) destroy_string,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		(type_stringify_t) util_expand_str,
+		NULL
+	));
+}
+
+
---
+++ src/modules/base/types/string.c
@@ -0,0 +1,30 @@
+/*
+ * Type Name:		string.c
+ * Version:		0.1
+ * Module Requirements:	type ; string ; modbase
+ * Description:		String Type
+ */
+
+#include <stdlib.h>
+
+#include <stutter/type.h>
+#include <stutter/lib/string.h>
+#include <stutter/modules/base.h>
+
+struct type_s *base_load_string(void)
+{
+	return(add_type(
+		"string",
+		0,
+		(type_create_t) recreate_string_real,
+		(type_destroy_t) destroy_string,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		(type_stringify_t) duplicate_string,
+		NULL
+	));
+}
+
+
