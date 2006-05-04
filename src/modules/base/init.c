a8fa9ae8e9fecadc5e88cccc34da3326eff556e9
trans <trans@jabberwocky.ca>
2006-05-04 03:57:11 +0000

    Modified for new variable system and added new types.

--- src/modules/base/init.c
+++ src/modules/base/init.c
@@ -36,28 +36,45 @@ int init_base(void)
 	int i = 0;
 	struct type_s *type;
 
+	/* Load Table Type */
+	if (!(type = base_load_table()))
+		return(-1);
+	add_variable(NULL, type, "base", 0, "");
+
+	/* Load String Type */
+	if (!(type = base_load_string()))
+		return(-1);
+
+	/* Load Format Type */
+	if (!(type = base_load_format()))
+		return(-1);
+
 	/* Load Status Type */
-	if (!(type = base_load_status()) || !type->create)
+	if (!(type = base_load_status()))
 		return(-1);
 
 	/* Load Time Type */
 	if (!(type = base_load_time()) || !type->create)
 		return(-1);
-	add_variable(type, BASE_NAMESPACE, "time", type->create("%s", BASE_TIME));
-	add_variable(type, BASE_NAMESPACE, "date", type->create("%s", BASE_DATE));
-	add_variable(type, BASE_NAMESPACE, "timestamp", type->create("%s", BASE_TIMESTAMP));
+	add_variable(NULL, type, "time", 0, "%s", BASE_TIME);
+	add_variable(NULL, type, "date", 0, "%s", BASE_DATE);
+	add_variable(NULL, type, "timestamp", 0, "%s", BASE_TIMESTAMP);
 
 	/* Load Command Type */
 	if (!(type = base_load_command()) || !type->create)
 		return(-1);
 	for (;base_commands[i].name;i++)
-		add_variable(type, BASE_NAMESPACE, base_commands[i].name, type->create("%r%p", base_commands[i].func, base_commands[i].ptr));
+		add_variable(NULL, type, base_commands[i].name, 0, "%r%p", base_commands[i].func, base_commands[i].ptr);
 
 	return(0);
 }
 
 int release_base(void)
 {
+	int i = 0;
+
+	for (;base_commands[i].name;i++)
+		remove_variable(NULL, NULL, base_commands[i].name);
 	return(0);
 }
 
