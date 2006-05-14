8a912acef7a57949cfd1507899928754a60a2898
trans <trans@jabberwocky.ca>
2006-05-14 16:33:15 +0000

    Added command to execute commands read from a file.

---
+++ src/modules/base/cmds/source.c
@@ -0,0 +1,56 @@
+/*
+ * Command Name:	source.c
+ * Version:		0.1
+ * Module Requirements:	type ; frontend ; variable ; string
+ * Description:		Parse a File as a List of Commands
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include CONFIG_H
+#include <stutter/type.h>
+#include <stutter/variable.h>
+#include <stutter/frontend.h>
+#include <stutter/lib/macros.h>
+#include <stutter/lib/string.h>
+
+int base_cmd_source(char *env, char *args)
+{
+	FILE *fptr;
+	char *str, *cmd;
+	struct variable_s *var;
+	char buffer[STRING_SIZE];
+
+	get_param_m(args, str, ' ');
+	if ((*str == '~') && (cmd = getenv("HOME"))) {
+		snprintf(buffer, STRING_SIZE, "%s%s", cmd, &str[1]);
+		str = buffer;
+	}
+
+	if (!(fptr = fopen(str, "r"))) {
+		snprintf(buffer, STRING_SIZE, "Error opening file: %s", str);
+		signal_emit("error.general", buffer);
+		return(-1);
+	}
+
+	while (str = fgets(buffer, STRING_SIZE, fptr)) {
+		if (cmd = strpbrk(str, "\n\r"))
+			*cmd = '\0';
+		if (*str != '\0') {
+			if (!strncmp(str, COMMAND_PREFIX, strlen(COMMAND_PREFIX)))
+				str = &str[strlen(COMMAND_PREFIX)];
+			get_param_m(str, cmd, ' ');
+			if ((var = find_variable(NULL, cmd)) && var->type->evaluate)
+				var->type->evaluate(var->value, str);
+			else
+				fe_print(fe_current_widget("window", NULL), UNKNOWN_COMMAND);
+		}
+	}
+	fclose(fptr);
+
+	return(0);
+}
+
+
