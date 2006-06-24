a8546936eedade30eada2ea7bf584ae94f4fea13
trans <trans@jabberwocky.ca>
2006-06-24 20:27:19 +0000

    Modified to not send the server as the first parameter

--- src/modules/irc/utils/dispatch.c
+++ src/modules/irc/utils/dispatch.c
@@ -8,30 +8,30 @@
 #include CONFIG_H
 #include <stutter/modules/irc.h>
 
-int irc_dispatch_msg(struct irc_server *server, struct irc_msg *msg)
+int irc_dispatch_msg(char *env, struct irc_msg *msg)
 {
 	switch (msg->cmd) {
 		case IRC_MSG_NOTICE:
-			return(irc_msg_notice(msg->server, msg));
+			return(irc_msg_notice(env, msg));
 		case IRC_MSG_JOIN:
-			return(irc_msg_join(msg->server, msg));
+			return(irc_msg_join(env, msg));
 		case IRC_MSG_PART:
-			return(irc_msg_part(msg->server, msg));
+			return(irc_msg_part(env, msg));
 		case IRC_MSG_PRIVMSG:
-			return(irc_msg_privmsg(msg->server, msg));
+			return(irc_msg_privmsg(env, msg));
 		case IRC_RPL_NAMREPLY:
-			return(irc_msg_names(msg->server, msg));
+			return(irc_msg_names(env, msg));
 		case IRC_RPL_TOPIC:
 		case IRC_MSG_TOPIC:
-			return(irc_msg_topic(msg->server, msg));
+			return(irc_msg_topic(env, msg));
 		case IRC_MSG_KICK:
-			return(irc_msg_kick(msg->server, msg));
+			return(irc_msg_kick(env, msg));
 		case IRC_MSG_NICK:
-			return(irc_msg_nick(msg->server, msg));
+			return(irc_msg_nick(env, msg));
 		case IRC_MSG_QUIT:
-			return(irc_msg_quit(msg->server, msg));
+			return(irc_msg_quit(env, msg));
 		default:
-			return(irc_msg_default(msg->server, msg));
+			return(irc_msg_default(env, msg));
 	}
 
 	return(0);
