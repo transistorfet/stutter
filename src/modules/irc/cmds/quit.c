fec467319e3b963b6234df9fd27cb1655391400b
trans <trans@jabberwocky.ca>
2006-06-24 17:45:37 +0000

    Changed fe_terminate to fe_quit

--- src/modules/irc/cmds/quit.c
+++ src/modules/irc/cmds/quit.c
@@ -12,11 +12,12 @@ int irc_cmd_quit(char *env, char *args)
 {
 	struct irc_msg *msg;
 
+	// TODO remove this command and make this broadcast message a signal handler
 	if (!(msg = irc_create_msg(IRC_MSG_QUIT, NULL, NULL, 1, (*args != '\0') ? args : IRC_QUIT_MSG)))
 		return(-1);
 	irc_broadcast_msg(msg);
 	irc_destroy_msg(msg);
-	fe_terminate(0);
+	fe_quit(0);
 	return(0);
 }
 
