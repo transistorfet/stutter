5b623800f4e0fccfa691a1bc0cc31547beb0baf9
trans <trans@jabberwocky.ca>
2006-06-25 01:23:40 +0000

    Modified to distinguish between public and private messages.

--- src/modules/irc/msgs/privmsg.c
+++ src/modules/irc/msgs/privmsg.c
@@ -19,22 +19,32 @@ int irc_msg_privmsg(char *env, struct irc_msg *msg)
 	char buffer[STRING_SIZE];
 	struct irc_channel *channel;
 
-	// TODO modify so privmsgs to individuals are still printed but using a differnt format
-	if (!(channel = irc_find_channel(msg->server->channels, msg->params[0])) && ((msg->params[0][0] != '#') && !(channel = irc_current_channel())))
-		return(-1);
+	if (strchr(msg->text, 0x01)) {
+		// TODO emit this as a CTCP message
+		// emit_signal("irc_dispatch_ctcp", NULL, msg);
+		// TODO do you still print the non-CTCP part?
 
-	if ((msg->text[0] == 0x01) && !strncmp(&msg->text[1], "ACTION", 6)) {
-		msg->text = &msg->text[8];
-		msg->text[strlen(msg->text) - 1] = '\0';
-		if (irc_format_msg(msg, msg->nick ? IRC_FMT_ACTION : IRC_FMT_ACTION_SELF, buffer, STRING_SIZE) < 0)
-			return(-1);
+		// TODO remove this and replace with a ctcp handler
+		if (!strncmp(&msg->text[1], "ACTION", 6)) {
+			msg->text = &msg->text[8];
+			msg->text[strlen(msg->text) - 1] = '\0';
+			if (irc_format_msg(msg, msg->nick ? IRC_FMT_ACTION : IRC_FMT_ACTION_SELF, buffer, STRING_SIZE) < 0)
+				return(-1);
+			fe_print(channel->window, buffer);
+		}
 	}
 	else {
-		if (irc_format_msg(msg, msg->nick ? IRC_FMT_PRIVMSG : IRC_FMT_PRIVMSG_SELF, buffer, STRING_SIZE) < 0)
+		if (!(channel = irc_find_channel(msg->server->channels, msg->params[0]))) {
+			if ((msg->params[0][0] == '#') || !(channel = irc_current_channel()))
+				return(-1);
+			if (irc_format_msg(msg, msg->nick ? IRC_FMT_PRIVATE_MSG : IRC_FMT_PRIVATE_MSG_SELF, buffer, STRING_SIZE) < 0)
+				return(-1);
+		}
+		else if (irc_format_msg(msg, msg->nick ? IRC_FMT_PUBLIC_MSG : IRC_FMT_PUBLIC_MSG_SELF, buffer, STRING_SIZE) < 0)
 			return(-1);
+		fe_print(channel->window, buffer);
 	}
 
-	fe_print(channel->window, buffer);
 	return(0);
 }
 
