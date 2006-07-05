90a785628d1e2af3c16d16bf1c03352df19f6006
trans <trans@jabberwocky.ca>
2006-07-05 03:06:54 +0000

    Added name validation macro and stringify and index inline functions.

--- include/stutter/variable.h
+++ include/stutter/variable.h
@@ -31,6 +31,44 @@ int remove_variable(struct variable_table_s *, struct type_s *, char *);
 struct variable_s *find_variable(struct variable_table_s *, char *);
 int traverse_variable_table(struct variable_table_s *, struct type_s *, type_traverse_func_t, void *);
 
+/**
+ * Returns true if the given char is a valid variable name char otherwise false.
+ */
+#define is_variable_char_m(ch)	\
+	(((str[i] >= 'A') && (str[i] <= 'Z')) || ((str[i] >= 'a') && (str[i] <= 'z')) || ((str[i] >= '0') && (str[i] <= '9')) || (str[i] == NAME_SEPARATOR) || (str[i] == '_') || (str[i] == '-'))
+
+/**
+ * Find the variable in the given table (or the root table if NULL) with the
+ * given name and index it with the given index returning the variable found
+ * or NULL on error.
+ * 
+ */
+static inline struct variable_s *index_variable(struct variable_table_s *table, char *name, char *index)
+{
+	struct variable_s *variable;
+
+	if (!(variable = find_variable(table, name)) || !variable->type->index)
+		return(NULL);
+	return(variable->type->index(variable->value, index));
+}
+
+/**
+ * Find the variable in the given table (or the root table if NULL) with the
+ * given name and stringify it storing the string in the given buffer up to
+ * the maximum number of characters.  The number of characters written to the
+ * buffer is returned or -1 on error.
+ */
+static inline int stringify_variable(struct variable_table_s *table, char *name, char *buffer, int max)
+{
+	struct variable_s *variable;
+
+	if (!(variable = find_variable(table, name)) || !variable->type->stringify)
+		return(-1);
+	return(variable->type->stringify(variable->value, buffer, max));
+}
+
+
+
 #endif
 
 
