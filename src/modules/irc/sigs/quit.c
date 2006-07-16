746b7438efde0675de3513323e73b47c3ad1f9f2
trans <trans@jabberwocky.ca>
2006-07-16 16:35:54 +0000

    Modified for new irc_send_msg function

--- src/modules/irc/sigs/quit.c
+++ src/modules/irc/sigs/quit.c
@@ -15,7 +15,6 @@ int irc_sig_quit(void *ptr, char *args)
 	if (!(msg = irc_create_msg(IRC_MSG_QUIT, NULL, NULL, 1, (*args != '\0') ? args : IRC_QUIT_MSG)))
 		return(-1);
 	irc_broadcast_msg(msg);
-	irc_destroy_msg(msg);
 	return(0);
 }
 
