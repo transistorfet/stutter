f46d4858261e32dae7b72e7aea0155a18e058c5a
trans <trans@jabberwocky.ca>
2006-07-16 16:36:52 +0000

    Modified to set the connected bit in the server struct when the appropriate messages are received.

--- src/modules/irc/sigs/dispatch.c
+++ src/modules/irc/sigs/dispatch.c
@@ -11,6 +11,12 @@
 int irc_dispatch_msg(char *env, struct irc_msg *msg)
 {
 	switch (msg->cmd) {
+		case 001:
+		case 002:
+		case 003:
+		case 004:
+			msg->server->bitflags |= IRC_SBF_CONNECTED;
+			return(irc_msg_default(env, msg));
 		case IRC_MSG_NOTICE:
 			return(irc_msg_notice(env, msg));
 		case IRC_MSG_JOIN:
