ed529afb2574aeba21941c7a0ab52fb0de1a9315
trans <trans@jabberwocky.ca>
2006-07-27 16:50:42 +0000

    Fixed bug where the message was being mangled before it was sent.

--- src/modules/irc/msgs/ctcp/action.c
+++ src/modules/irc/msgs/ctcp/action.c
@@ -17,12 +17,14 @@
  */
 int irc_msg_ctcp_action(char *env, struct irc_msg *msg)
 {
+	int pos;
 	char buffer[STRING_SIZE];
 	struct irc_channel *channel;
 
 	if (!strncmp_icase(&msg->text[1], "ACTION", 6)) {
+		pos = strlen(msg->text) - 1;
+		msg->text[pos] = '\0';
 		msg->text = &msg->text[8];
-		msg->text[strlen(msg->text) - 1] = '\0';
 		if (!(channel = irc_find_channel(msg->server->channels, msg->params[0]))) {
 			if ((msg->params[0][0] == '#') || !(channel = irc_current_channel()))
 				return(-1);
@@ -32,6 +34,8 @@ int irc_msg_ctcp_action(char *env, struct irc_msg *msg)
 		else if (irc_format_msg(msg, msg->nick ? IRC_FMT_PUBLIC_ACTION : IRC_FMT_PUBLIC_ACTION_SELF, buffer, STRING_SIZE) < 0)
 			return(-1);
 		fe_print(channel->window, buffer);
+		msg->text = &msg->text[-8];
+		msg->text[pos] = '\x01';
 	}
 	return(0);
 }
