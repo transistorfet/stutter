bb812c4ee920d8f3177ace2009e98dc609869752
trans <trans@jabberwocky.ca>
2006-07-29 22:35:35 +0000

    Added handler to print a message to the current window.

---
+++ src/modules/irc/msgs/current.c
@@ -0,0 +1,25 @@
+/*
+ * Module Name:		current.c
+ * Version:		0.1
+ * Module Requirements:	frontend ; modirc
+ * Description:		Generic Print-to-Current-Window Notification Message
+ */
+
+#include CONFIG_H
+#include <stutter/frontend.h>
+#include <stutter/modules/irc.h>
+
+int irc_msg_current(char *env, struct irc_msg *msg)
+{
+	char buffer[STRING_SIZE];
+	struct irc_channel *channel;
+
+	if (!(channel = irc_current_channel()))
+		return(-1);
+	if (irc_format_msg(msg, env ? env : IRC_FMT_CURRENT, buffer, STRING_SIZE) < 0)
+		return(-1);
+	fe_print(channel->window, buffer);
+	return(0);
+}
+
+
