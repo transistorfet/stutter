1b86ad72269f2112100aacb104ad4b369dd5f4de
trans <trans@jabberwocky.ca>
2006-07-29 22:36:03 +0000

    Modified to optionally take the format as the env parameter.

--- src/modules/irc/msgs/default.c
+++ src/modules/irc/msgs/default.c
@@ -13,7 +13,7 @@ int irc_msg_default(char *env, struct irc_msg *msg)
 {
 	char buffer[STRING_SIZE];
 
-	if (irc_format_msg(msg, IRC_FMT_DEFAULT, buffer, STRING_SIZE) < 0)
+	if (irc_format_msg(msg, env ? env : IRC_FMT_DEFAULT, buffer, STRING_SIZE) < 0)
 		return(-1);
 	fe_print(msg->server->status->window, buffer);
 	return(0);
