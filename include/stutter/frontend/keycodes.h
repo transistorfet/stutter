a687038e49bab12527a7d1a7b361d0924d69740a
trans <trans@jabberwocky.ca>
2006-08-12 03:24:29 +0000

    Added common key assignments.

---
+++ include/stutter/frontend/keycodes.h
@@ -0,0 +1,36 @@
+/*
+ * Header Name:		keycodes.h
+ * Version:		0.1
+ * Description:		Key Code Definitions
+ */
+
+#ifndef _FRONTEND_KEYCODES_H
+#define _FRONTEND_KEYCODES_H
+
+#define KC_ENTER		0x0A
+#define KC_BACKSPACE		0x08
+
+#define KC_DOWN			0x102
+#define KC_UP			0x103
+#define KC_LEFT			0x104
+#define KC_RIGHT		0x105
+
+#define KC_HOME			0x106
+//#define KC_END			0x106
+//#define KC_PAGEUP		0x107
+//#define KC_PAGEDOWN		0x108
+#define KC_F1			0x109
+#define KC_F2			0x10A
+#define KC_F3			0x10B
+#define KC_F4			0x10C
+#define KC_F5			0x10D
+#define KC_F6			0x10E
+#define KC_F7			0x10F
+#define KC_F8			0x110
+#define KC_F9			0x111
+#define KC_F10			0x112
+#define KC_F11			0x113
+#define KC_F12			0x114
+
+#endif
+
