b6ffd196df9778f6b3bea76f124ababee070567f
trans <trans@jabberwocky.ca>
2006-09-04 20:39:48 +0000

    Added mode command.

---
+++ src/modules/irc/cmds/mode.c
@@ -0,0 +1,27 @@
+/*
+ * Command Name:	mode.c
+ * Version:		0.1
+ * Module Requirements:	modirc
+ * Description:		Mode Command
+ */
+
+#include <stutter/lib/macros.h>
+#include <stutter/modules/irc.h>
+
+int irc_cmd_mode(char *env, char *args)
+{
+	char *channel;
+	struct irc_msg *msg;
+	struct irc_server *server;
+
+	if (*args == '\0' || !(server = irc_current_server()))
+		return(-1);
+	get_param_m(args, channel, ' ');
+	if (*args == '\0')
+		return(-1);
+	if (msg = irc_create_msg(IRC_MSG_MODE, NULL, NULL, 2, channel, args))
+		irc_send_msg(server, msg);
+	return(0);
+}
+
+
