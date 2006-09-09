ba9b0f6923b34bc324666479e1819cefe6cc7f19
trans <trans@jabberwocky.ca>
2006-09-09 18:57:31 +0000

    Modified to only search the last word for a name and if the search doesn't start at the beginning of the input line, then a : is not added to after the name when a match is found.

--- src/modules/irc/cmds/complete.c
+++ src/modules/irc/cmds/complete.c
@@ -9,32 +9,66 @@
 #include <stutter/lib/macros.h>
 #include <stutter/modules/irc.h>
 
-static int size;
-static int irc_cmd_complete_traverse(struct irc_user *, char *);
+struct irc_complete_info_s {
+	int find_next : 1;
+	int no_colon : 1;
+	int return_next : 1;
+	int size;
+	int max;
+	char *str;
+};
+
+static int irc_cmd_complete_traverse(struct irc_user *, struct irc_complete_info_s *);
 
 int irc_cmd_complete(char *env, char *args)
 {
+	int i;
+	char *str;
 	void *input;
 	char buffer[STRING_SIZE];
 	struct irc_channel *channel;
+	struct irc_complete_info_s info = { 0, 0, 0, 0, 0 };
 
 	if (!(channel = irc_current_channel()) || !(input = fe_current_widget("input", NULL)))
 		return(-1);
 	fe_read(input, buffer, STRING_SIZE);
-	if (((size = strlen(buffer)) > 2) && (buffer[size - 2] == ':'))
-		size -= 2;
-	irc_traverse_user_list(channel->users, (traverse_t) irc_cmd_complete_traverse, buffer);
+
+	info.str = buffer;
+	info.size = strlen(buffer);
+	info.max = STRING_SIZE;
+
+	if (info.size > 2) {
+		if ((info.str[info.size - 1] == ' ') && (info.str[info.size - 2] != ' ')) {
+			info.find_next = 1;
+			info.size -= (info.str[info.size - 2] == ':') ? 2 : 1;
+		}
+		for (i = info.size - 1;i >= 0;i--) {
+			if (buffer[i] == ' ') {
+				info.str = &buffer[i + 1];
+				info.no_colon = 1;
+				info.max = STRING_SIZE - i;
+				info.size -= (i + 1);
+				break;
+			}
+		}
+	}
+
+	irc_traverse_user_list(channel->users, (traverse_t) irc_cmd_complete_traverse, &info);
 	fe_print(input, buffer);
 	return(0);
 }
 
-static int irc_cmd_complete_traverse(struct irc_user *user, char *buffer)
+static int irc_cmd_complete_traverse(struct irc_user *user, struct irc_complete_info_s *info)
 {
-	if ((size == -1) || !strncmp_icase(user->nick, buffer, size)) {
-		if ((size != -1) && (buffer[size] == ':'))
-			size = -1;
+	if (info->return_next || !strncmp_icase(user->nick, info->str, info->size)) {
+		if (info->find_next && !info->return_next)
+			info->return_next = 1;
+		else if (info->no_colon) {
+			snprintf(info->str, info->max, "%s ", user->nick);
+			return(1);
+		}
 		else {
-			sprintf(buffer, "%s: ", user->nick);
+			snprintf(info->str, info->max, "%s: ", user->nick);
 			return(1);
 		}
 	}
