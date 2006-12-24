7c63fb0c7bbf17c87b7d708590582b7f0efd20db
trans <trans@jabberwocky.ca>
2006-12-24 21:54:51 +0000

    Added common frontend header file.

---
+++ include/stutter/frontend/common.h
@@ -0,0 +1,21 @@
+/*
+ * Header Name:		common.h
+ * Version:		0.1
+ * Description:		Common Frontend Header
+ */
+
+#ifndef _STUTTER_FRONTEND_COMMON_H
+#define _STUTTER_FRONTEND_COMMON_H
+
+#include <stutter/type.h>
+
+/* Commands */
+int fe_common_cmd_insert(char *, char *);
+
+/* Types */
+struct type_s *fe_common_load_colour(void);
+struct type_s *fe_common_load_command(void);
+
+#endif
+
+
