1ab63de89b2366dea9a9a5260449e8068a0a75c6
trans <trans@jabberwocky.ca>
2006-12-30 02:15:50 +0000

    Added -O2 flag.

--- config.mk
+++ config.mk
@@ -5,7 +5,7 @@ MODULES = $(ROOT)/src/modules/base/modbase.a \
 
 CONFIG_H = ../config.h
 ARFLAGS = rc
-CFLAGS += -g3 -DDEBUG -DCONFIG_H='<$(CONFIG_H)>' -I$(ROOT)/include
+CFLAGS += -O2 -g3 -DDEBUG -DCONFIG_H='<$(CONFIG_H)>' -I$(ROOT)/include
 
 CC	= gcc
 CXX	= g++
