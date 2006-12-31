e97cd15f35d87bea5dcc62cbcfa87a3575268e68
trans <trans@jabberwocky.ca>
2006-12-31 00:27:09 +0000

    Modified to parse irc colouring into the new string format.

--- src/modules/irc/utils/format.c
+++ src/modules/irc/utils/format.c
@@ -12,14 +12,18 @@
 #include CONFIG_H
 #include <stutter/utils.h>
 #include <stutter/lib/string.h>
+#include <stutter/lib/macros.h>
 #include <stutter/modules/irc.h>
 #include <stutter/modules/base.h>
+#include <stutter/frontend/surface.h>
 
 #define msg_copy_str_m(buffer, str, count, max)				\
 	if (str) {							\
 		strncpy(&buffer[count], str, (max - count - 1));	\
 		count += strlen(str);					\
 	}
+
+static int irc_format_parse_text(char *, char *, int);
 
 /**
  * Format the data in the given message (and any variable parameters) using
@@ -78,8 +82,8 @@ int irc_format_msg(struct irc_msg *msg, char *fmt, char *buffer, int max)
 						msg_copy_str_m(buffer, msg->host, j, max);
 					break;
 				case 'M':
-					if (msg)
-						msg_copy_str_m(buffer, msg->text, j, max);
+					if (msg && msg->text)
+						j += irc_format_parse_text(msg->text, &buffer[j], max - j + 1);
 					break;
 				case 'P':
 					if (msg) {
@@ -105,4 +109,75 @@ int irc_format_msg(struct irc_msg *msg, char *fmt, char *buffer, int max)
 	buffer[j] = '\0';
 	return(j);
 }
+
+/*** Local Functions ***/
+
+static int irc_format_parse_text(char *str, char *buffer, int max)
+{
+	attrib_t attrib;
+	int i = 0, j = 0, write = 0;
+	attrib_t def_attrib = { SA_METHOD_OR, SA_NORMAL, { SC_ENC_MAPPING, SC_MAP_CURRENT_COLOUR }, { SC_ENC_MAPPING, SC_MAP_CURRENT_COLOUR } };
+
+	max--;
+	attrib = def_attrib;
+	while ((str[i] != '\0') && (j < max)) {
+		switch (str[i]) {
+			case 0x02: {
+				attrib.method = SA_METHOD_TOGGLE;
+				attrib.style ^= SA_BOLD;
+				write = 1;
+				i++;
+				break;
+			}
+			case 0x03: {
+				i++;
+				if (!is_number_char_m(str[i])) {
+					attrib.fg.colour = SC_MAP_DEFAULT_COLOUR;
+					attrib.bg.colour = SC_MAP_DEFAULT_COLOUR;
+				}
+				else {
+					attrib.fg.colour = str[i++] - 0x30;
+					if (is_number_char_m(str[i]))
+						attrib.fg.colour = (attrib.fg.colour * 10) + (str[i++] - 0x30);
+					if ((str[i] == ',') && ++i && is_number_char_m(str[i])) {
+						attrib.bg.colour = str[i++] - 0x30;
+						if (is_number_char_m(str[i]))
+							attrib.bg.colour = (attrib.bg.colour * 10) + (str[i++] - 0x30);
+					}
+				}
+				write = 1;
+				break;
+			}
+			case 0x0F: {
+				attrib.method = SA_METHOD_SET;
+				attrib.style = SA_NORMAL;
+				write = 1;
+				i++;
+				break;
+			}
+			case 0x16: {
+				attrib.method = SA_METHOD_TOGGLE;
+				attrib.style ^= SA_INVERSE;
+				write = 1;
+				i++;
+				break;
+			}
+			default: {
+				if (write) {
+					write = 0;
+					if (max - j < sizeof(attrib_t) + 1)
+						break;
+					buffer[j++] = '\x01';
+					j += util_encode_bytes((unsigned char *) &attrib, sizeof(attrib_t), &buffer[j], max - j + 1);
+					attrib = def_attrib;
+				}
+				buffer[j++] = str[i++];
+				break;
+			}
+		}
+	}
+	buffer[j] = '\0';
+	return(j);
+}
+
 
