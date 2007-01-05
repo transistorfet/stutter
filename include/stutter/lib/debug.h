2d8c23954944769cf610906e482c718e4efe7bdd
trans <trans@jabberwocky.ca>
2007-01-05 04:51:50 +0000

    Modified to take full variable parameters to allow printing of values.

--- include/stutter/lib/debug.h
+++ include/stutter/lib/debug.h
@@ -10,24 +10,24 @@
 #include <stdio.h>
 
 #ifdef DEBUG
-	#define DEBUG_LOG(file, str) {			\
-		FILE *debug_fptr;			\
-		if (debug_fptr = fopen(file, "a")) {	\
-			fprintf(debug_fptr, "%s", str);	\
-			fclose(debug_fptr);		\
-		}					\
+	#define DEBUG_LOG(file, ...) {				\
+		FILE *debug_fptr;				\
+		if (debug_fptr = fopen(file, "a")) {		\
+			fprintf(debug_fptr, __VA_ARGS__);	\
+			fclose(debug_fptr);			\
+		}						\
 	}
 #else
 	#define DEBUG_LOG(file, str)
 #endif
 
 #ifdef DEBUG
-	#define DEBUG_LOG_LIMITED(file, str, num) {	\
-		FILE *debug_fptr;			\
-		if (debug_fptr = fopen(file, "a")) {	\
-			fwrite(str, num, 1, debug_fptr); \
-			fclose(debug_fptr);		\
-		}					\
+	#define DEBUG_LOG_LIMITED(file, str, num) {		\
+		FILE *debug_fptr;				\
+		if (debug_fptr = fopen(file, "a")) {		\
+			fwrite(str, num, 1, debug_fptr);	\
+			fclose(debug_fptr);			\
+		}						\
 	}
 #else
 	#define DEBUG_LOG_LIMITED(file, str, num)
