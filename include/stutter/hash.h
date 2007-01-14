375f7acffef15784f70a0c38b42183e0a3348b4b
trans <trans@jabberwocky.ca>
2007-01-14 05:11:17 +0000

    Rewrote the inline functions to use the define method used in queue.h

--- include/stutter/hash.h
+++ include/stutter/hash.h
@@ -14,18 +14,6 @@ typedef unsigned int (*hash_t)(void *);
 
 /*** Virtual Interface ***/
 
-#ifndef HASH_LOAD_FACTOR
-#define HASH_LOAD_FACTOR	0.75
-#endif
-
-#ifndef HASH_GROW_FACTOR
-#define HASH_GROW_FACTOR	1.75
-#endif
-
-#ifndef HASH_INITIAL_SIZE
-#define HASH_INITIAL_SIZE	10
-#endif
-
 #define hash_node_v(node_type)					\
 	struct {						\
 		struct node_type *next;				\
@@ -98,19 +86,6 @@ typedef unsigned int (*hash_t)(void *);
 	}								\
 }
 
-#define hash_traverse_list_v(list, field, body) {			\
-	int hashi;							\
-	typeof((list).table[0]) cur;					\
-									\
-	for (hashi = 0;hashi < (list).size;hashi++) {			\
-		cur = (list).table[hashi];				\
-		while (cur) {						\
-			body;						\
-			cur = cur->field.next;				\
-		}							\
-	}								\
-}
-
 #define hash_rehash_v(list, field, newsize, hash_func, failed_body) {					\
 	int i, hash, oldsize;										\
 	typeof((list).table) newtable;									\
@@ -139,21 +114,15 @@ typedef unsigned int (*hash_t)(void *);
 	}												\
 }
 
-#define hash_destroy_list_v(list, field, body) {	\
-	int i;						\
-	typeof((list).table[0]) cur;			\
-	typeof((list).table[0]) tmp;			\
-							\
-	for (i = 0;i < (list).size;i++) {		\
-		cur = (list).table[i];			\
-		while (cur) {				\
-			tmp = cur->field.next;		\
-			body;				\
-			cur = tmp;			\
-		}					\
-	}						\
-	/*memory_free((list).table);*/			\
-}
+#define hash_foreach_v(list, field, inc, cur)				\
+	for (inc = 0;inc < (list).size;inc++)				\
+		for (cur = (list).table[inc];cur;cur = cur->field.next)
+
+#define hash_foreach_safe_v(list, field, inc, cur, tmp)			\
+	for (inc = 0;inc < (list).size;inc++)				\
+		for (cur = (list).table[inc];cur && ((tmp = cur->field.next), 1);cur = tmp)
+
+#define hash_next_v(field, node)	( (node)->field.next )
 
 #define hash_size_v(list)		(list).size
 #define hash_entries_v(list)		(list).entries
@@ -200,76 +169,182 @@ static inline unsigned int sdbm_hash_icase(char *str) {
 
 /*** Inline Interface ***/
 
+#define DEFINE_HASH_INIT_TABLE(hash_name, list_type, node_type, field)			\
+	static inline int hash_name##_init_table(list_type *table, int init_size) {	\
+		hash_init_v(table->field, init_size);					\
+		return(0);								\
+	}
+
+#define DEFINE_HASH_RELEASE_TABLE(hash_name, list_type, node_type, field, release)	\
+	static inline void hash_name##_release_table(list_type *table) {		\
+		int i;									\
+		node_type *cur, *tmp;							\
+											\
+		hash_foreach_safe_v(table->field, field, i, cur, tmp) {			\
+			release(table, cur);						\
+			memory_free(cur);						\
+		}									\
+		hash_release_v(table->field);						\
+	}
+
+#define DEFINE_HASH_CREATE_TABLE(hash_name, list_type, node_type, field)	\
+	static inline list_type *hash_name##_create_table(int init_size) {	\
+		list_type *table;						\
+										\
+		if (!(table = (list_type *) memory_alloc(sizeof(list_type))))	\
+			return(NULL);						\
+		hash_init_v(table->field, init_size);				\
+		return(table);							\
+	}
+
+#define DEFINE_HASH_DESTROY_TABLE(hash_name, list_type, node_type, field, release)	\
+	static inline int hash_name##_destroy_table(list_type *table) {		\
+		int i;									\
+		node_type *cur, *tmp;							\
+											\
+		hash_foreach_safe_v(table->field, field, i, cur, tmp) {			\
+			release(table, cur);						\
+			memory_free(cur);						\
+		}									\
+		hash_release_v(table->field);						\
+		memory_free(table);							\
+		return(0);								\
+	}
+
+#define DEFINE_HASH_CREATE_NODE(hash_name, list_type, node_type, field)				\
+	static inline node_type *hash_name##_create_node(int size) {				\
+		node_type *node;								\
+												\
+		if (!(node = (node_type *) memory_alloc(size ? size : sizeof(node_type))))	\
+			return(NULL);								\
+		return(node);									\
+	}
+
+#define DEFINE_HASH_DESTROY_NODE(hash_name, list_type, node_type, field, release)		\
+	static inline int hash_name##_destroy_node(list_type *table, node_type *node) {		\
+		release(table, node);								\
+		memory_free(node);								\
+		return(0);									\
+	}
+
+#define DEFINE_HASH_ADD_NODE(hash_name, list_type, node_type, field, keyfield, hashfunc, compare, hashload, hashgrowth)		\
+	static inline int hash_name##_add_node(list_type *table, node_type *node) {					\
+		hash_add_node_v(table->field, field, node, hashfunc(table, node->keyfield) % hash_size_v(table->field));			\
+		if (hash_load_v(table->field) > hashload)								\
+			hash_rehash_v(table->field, field, (hash_size_v(table->field) * hashgrowth), hashfunc(table, cur->keyfield) % hash_size_v(table->field), return(-1));	\
+		return(0);												\
+	}
+
+#define DEFINE_HASH_REMOVE_NODE(hash_name, list_type, node_type, field, keyfield, hashfunc, compare)	\
+	static inline node_type *hash_name##_remove_node(list_type *table, void *key) {			\
+		node_type *node;									\
+													\
+		hash_remove_node_v(table->field, field, node, hashfunc(table, key) % hash_size_v(table->field), compare(cur, key));	\
+		return(node);										\
+	}
+
+#define DEFINE_HASH_DELETE_NODE(hash_name, list_type, node_type, field, keyfield, release, hashfunc, compare)	\
+	static inline int hash_name##_delete_node(list_type *table, void *key) {				\
+		node_type *node;										\
+														\
+		hash_remove_node_v(table->field, field, node, hashfunc(table, key) % hash_size_v(table->field), compare(cur, key));	\
+		if (!node)											\
+			return(-1);										\
+		release(table, node);										\
+		memory_free(node);										\
+		return(0);											\
+	}
+
+#define DEFINE_HASH_FIND_NODE(hash_name, list_type, node_type, field, keyfield, hashfunc, compare)	\
+	static inline node_type *hash_name##_find_node(list_type *table, void *key) {			\
+		node_type *node;									\
+													\
+		hash_find_node_v(table->field, field, node, hashfunc(table, key) % hash_size_v(table->field), compare(cur, key));	\
+		return(node);										\
+	}
+
+#define DEFINE_HASH_NEXT_NODE(hash_name, list_type, node_type, field)		\
+	static inline node_type *hash_name##_next_node(node_type *node) {	\
+		return(node ? hash_next_v(field, node) : NULL);			\
+	}
+
+#define DEFINE_HASH_TABLE(hash_name, list_type, node_type, field, keyfield, release, hashfunc, compare, hashload, hashgrowth)	\
+	DEFINE_HASH_INIT_TABLE(hash_name, list_type, node_type, field)								\
+	DEFINE_HASH_RELEASE_TABLE(hash_name, list_type, node_type, field, release)						\
+	DEFINE_HASH_CREATE_TABLE(hash_name, list_type, node_type, field)							\
+	DEFINE_HASH_DESTROY_TABLE(hash_name, list_type, node_type, field, release)						\
+	DEFINE_HASH_CREATE_NODE(hash_name, list_type, node_type, field)								\
+	DEFINE_HASH_DESTROY_NODE(hash_name, list_type, node_type, field, release)						\
+	DEFINE_HASH_ADD_NODE(hash_name, list_type, node_type, field, keyfield, hashfunc, compare, hashload, hashgrowth)		\
+	DEFINE_HASH_REMOVE_NODE(hash_name, list_type, node_type, field, keyfield, hashfunc, compare)				\
+	DEFINE_HASH_DELETE_NODE(hash_name, list_type, node_type, field, keyfield, release, hashfunc, compare)			\
+	DEFINE_HASH_FIND_NODE(hash_name, list_type, node_type, field, keyfield, hashfunc, compare)				\
+	DEFINE_HASH_NEXT_NODE(hash_name, list_type, node_type, field)
+
+
+/*** Predefined Generic String-Indexed Hash Table ***/
+
+#ifndef HASH_LOAD_FACTOR
+#define HASH_LOAD_FACTOR	0.75
+#endif
+
+#ifndef HASH_GROWTH_FACTOR
+#define HASH_GROWTH_FACTOR	1.75
+#endif
+
 struct hash_node_s {
+	hash_node_v(hash_node_s) h;
+	char *key;
 	void *ptr;
-	hash_node_v(hash_node_s) list;
 };
 
 struct hash_table_s {
-	hash_t hash;
-	compare_t compare;
-	hash_list_v(hash_node_s) list;
+	hash_list_v(hash_node_s) h;
+	destroy_t destroy;
 };
 
-static inline struct hash_table_s *hash_create_list(int init_size, hash_t hash, compare_t compare)
+static inline int hash_init_node(struct hash_table_s *table, struct hash_node_s *node, char *key, void *ptr)
 {
-	struct hash_table_s *table;
-
-	if (!(table = (struct hash_table_s *) memory_alloc(sizeof(struct hash_table_s))))
-		return(NULL);
-	init_size = init_size ? init_size : HASH_INITIAL_SIZE;
-	hash_init_v(table->list, init_size);
-	table->hash = hash;
-	table->compare = compare;
-	return(table);
-}
-
-static inline void hash_destroy_list(struct hash_table_s *table, destroy_t destroy)
-{
-	hash_destroy_list_v(table->list, list,
-		destroy(cur->ptr);
-		memory_free(cur);
-	);
-	hash_release_v(table->list);
-	memory_free(table);
-}
-
-static inline int hash_add_node(struct hash_table_s *table, void *key, void *ptr)
-{
-	struct hash_node_s *node;
-
-	if (!(node = (struct hash_node_s *) memory_alloc(sizeof(struct hash_node_s))))
-		return(-1);
+	node->key = key;
 	node->ptr = ptr;
-	hash_add_node_v(table->list, list, node, table->hash(key) % hash_size_v(table->list));
-	if (hash_load_v(table->list) > HASH_LOAD_FACTOR)
-		hash_rehash_v(table->list, list, (hash_size_v(table->list) * 1.75), table->hash(key) % hash_size_v(table->list), NULL);
 	return(0);
 }
 
-static inline void *hash_remove_node(struct hash_table_s *table, void *key)
+static inline int hash_release_node(struct hash_table_s *table, struct hash_node_s *node)
 {
-	void *ptr;
-	struct hash_node_s *node;
-
-	hash_remove_node_v(table->list, list, node, table->hash(key) % hash_size_v(table->list), table->compare(cur->ptr, key));
-	if (!node)
-		return(NULL);
-	ptr = node->ptr;
-	memory_free(node);
-	return(ptr);
+	if (table->destroy)
+		table->destroy(node->ptr);
+	return(0);
 }
 
-static inline void *hash_find_node(struct hash_table_s *table, void *key)
-{
-	struct hash_node_s *node;
+#define hash_hashfunc(list, key)	\
+	sdbm_hash(key)
 
-	hash_find_node_v(table->list, list, node, table->hash(key) % hash_size_v(table->list), table->compare(cur->ptr, key));
-	if (!node)
+#define hash_compare_node(node, ptr)	\
+	(!strcmp(node->key, key))
+
+DEFINE_HASH_TABLE(hash, struct hash_table_s, struct hash_node_s, h, key, hash_release_node, hash_hashfunc, hash_compare_node, HASH_LOAD_FACTOR, HASH_GROWTH_FACTOR)
+
+static inline struct hash_table_s *create_hash_table(int init_size, destroy_t destroy)
+{
+	struct hash_table_s *table;
+
+	if (!(table = hash_create_table(init_size)))
 		return(NULL);
-	return(node->ptr);
+	table->destroy = destroy;
+	return(table);
 }
 
+static inline int destroy_hash_table(struct hash_table_s *table)
+{
+	return(hash_destroy_table(table));
+}
+
+#define hash_foreach(table, inc, cur)	\
+	hash_foreach_v(table, h, inc, cur)
+
+#define hash_foreach_safe(table, inc, cur, tmp)	\
+	hash_foreach_safe_v(table, h, inc, cur, tmp)
 
 #endif
 
