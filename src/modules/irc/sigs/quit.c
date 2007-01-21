dc3ed54cf6c81066da1dcf5bd05cdc5ee1214033
trans <trans@jabberwocky.ca>
2007-01-21 23:27:04 +0000

    Modified for changes to msg.c

--- src/modules/irc/sigs/quit.c
+++ src/modules/irc/sigs/quit.c
@@ -12,7 +12,7 @@ int irc_sig_quit(void *ptr, void *index, char *args)
 {
 	struct irc_msg *msg;
 
-	if (!(msg = irc_create_msg(IRC_MSG_QUIT, NULL, NULL, 1, (*args != '\0') ? args : IRC_QUIT_MSG)))
+	if (!(msg = irc_create_msg(IRC_MSG_QUIT, NULL, NULL, 1, 0, (*args != '\0') ? args : IRC_QUIT_MSG)))
 		return(-1);
 	irc_broadcast_msg(msg);
 	return(0);
