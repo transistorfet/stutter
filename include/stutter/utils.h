0bf69e3fd8751f51c7f85742a54f27907d438a15
trans <trans@jabberwocky.ca>
2007-02-13 05:37:13 +0000

    Modified util_convert_key to use int * instead of char * as the key sequence.

--- include/stutter/utils.h
+++ include/stutter/utils.h
@@ -12,7 +12,7 @@ int util_itoa(int, char *, int);
 int util_encode_bytes(unsigned char *, int, char *, int);
 int util_decode_bytes(const char *, unsigned char *, int);
 int util_escape_char(const char *, char *);
-int util_convert_key(const char *, char *, int);
+int util_convert_key(const unsigned char *, int *, int);
 int util_expand_str(const char *, char *, int);
 int util_expand_variable(const char *, char *, int, int *);
 int util_emit_str(char *, void *, char *, ...);
