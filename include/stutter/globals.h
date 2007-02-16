146d9f693b1e1b6f4a1824c3865866527d4a6cc2
trans <trans@jabberwocky.ca>
2007-02-16 06:17:04 +0000

    Added callback struct, macro, and some IO condition constants.

--- include/stutter/globals.h
+++ include/stutter/globals.h
@@ -10,10 +10,23 @@
 #define ERR_OUT_OF_MEMORY		-10
 #define ERR_COMMAND_NOT_FOUND		-11
 
+#define IO_COND_READ			0x01
+#define IO_COND_WRITE			0x02
+#define IO_COND_ERROR			0x04
+
 typedef void (*destroy_t)(void *);
 typedef int (*compare_t)(void *, void *);
 typedef int (*traverse_t)(void *, void *);
 typedef int (*callback_t)(void *, void *);
+
+struct callback_s {
+	callback_t func;
+	void *ptr;
+};
+
+#define execute_callback_m(callback, arg)	\
+	if ((callback).func)			\
+		((callback).func)((callback).ptr, arg)
 
 #endif
 
