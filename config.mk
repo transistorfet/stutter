99532f5823fca54669e1e2fb16956975bd9ff784
trans <trans@jabberwocky.ca>
2007-03-11 23:24:29 +0000

    Turned on all warnings in debugging options.

--- config.mk
+++ config.mk
@@ -5,8 +5,8 @@ MODULES = $(ROOT)/src/modules/base/modbase.a \
 
 CONFIG_H = ../config.h
 ARFLAGS = rc
-#CFLAGS += -O2 -DCONFIG_H='<$(CONFIG_H)>' -I$(ROOT)/include
-CFLAGS += -g3 -DDEBUG -DCONFIG_H='<$(CONFIG_H)>' -I$(ROOT)/include
+CFLAGS += -O2 -DCONFIG_H='<$(CONFIG_H)>' -I$(ROOT)/include
+#CFLAGS += -g3 -DDEBUG -Wall -DCONFIG_H='<$(CONFIG_H)>' -I$(ROOT)/include
 
 CC	= gcc
 CXX	= g++
