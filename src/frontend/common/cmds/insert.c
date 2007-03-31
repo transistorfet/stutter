2dc53d082466b85d029fb2be1ca7479a9852436b
trans <trans@jabberwocky.ca>
2007-03-31 04:08:23 +0000

    Changed fe_current_widget to fe_get_target.

--- src/frontend/common/cmds/insert.c
+++ src/frontend/common/cmds/insert.c
@@ -12,7 +12,7 @@
 
 int fe_common_cmd_insert(struct widget_s *widget, char *args)
 {
-	if ((*args == '\0') || (!widget && !(widget = fe_current_widget("input", NULL))))
+	if ((*args == '\0') || (!widget && !(widget = fe_get_focus("input"))))
 		return(-1);
 	widget_control(widget, WCC_INSERT_CHAR, args[0]);
 	return(0);
