04ab035c928fb0bba6d731951d87d115105d4567
trans <trans@jabberwocky.ca>
2007-10-17 04:28:36 +0000

    Modified config-macros.h into advice.h

---
+++ include/stutter/advice.h
@@ -0,0 +1,35 @@
+/*
+ * Header Name:	advice.h
+ * Description:	General Advice Macros Header
+ */
+
+#ifndef _STUTTER_ADVICE_H
+#define _STUTTER_ADVICE_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#include <stutter/utils.h>
+#include <stutter/signal.h>
+#include <stutter/frontend.h>
+
+#define DO_NOTHING() \
+	;
+
+#define EMIT_SIGNAL(obj, name, ptr) \
+	emit_signal(obj, name, ptr);
+
+#define EMIT_STRING(obj, name, ...) \
+	util_emit_str(obj, name, __VA_ARGS__);
+
+#define PRINT_TO_CURRENT(...) {						\
+	void *window;							\
+	char buffer[STRING_SIZE];					\
+	if (((window = fe_get_target(NULL, "text")))			\
+	    && (snprintf(buffer, STRING_SIZE, __VA_ARGS__) >= 0))	\
+		fe_print(window, buffer);				\
+}
+
+#endif
+
+
